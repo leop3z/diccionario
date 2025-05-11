3235ad3d7f7c9b6da56a4ab76eb775256e23067f
leop3z <lgpaz@fi.uba.ar>
2025-05-11 02:56:28 -0300

    Obtener funciona bien

--- abb.go
+++ abb.go
@@ -29,9 +29,9 @@ func (abb *abbDiccionario[K, V]) guardarRecursivo(nodo *nodoArbol[K, V], clave_n
 		return &nodoArbol[K, V]{clave: clave_nueva, dato: dato}
 	}
 	comparacion := abb.cmp(clave_nueva, nodo.clave)
-	if comparacion < 0 {
+	if comparacion > 0 {
 		nodo.hijo_izq = abb.guardarRecursivo(nodo.hijo_izq, clave_nueva, dato)
-	} else if comparacion > 0 {
+	} else if comparacion < 0 {
 		nodo.hijo_der = abb.guardarRecursivo(nodo.hijo_der, clave_nueva, dato)
 	} else {
 		nodo.dato = dato
@@ -57,10 +57,6 @@ func (abb *abbDiccionario[K, V]) busquedaRecursiva(nodo *nodoArbol[K, V], clave 
 	}
 }
 
-func (abb *abbDiccionario[K, V]) Pertenece(clave K) bool {
-	return abb.busquedaRecursiva(abb.raiz, clave)
-}
-
 func (abb *abbDiccionario[K, V]) obtenerNodo(nodo *nodoArbol[K, V], clave K) *nodoArbol[K, V] {
 	if nodo == nil {
 		return nil
@@ -75,6 +71,10 @@ func (abb *abbDiccionario[K, V]) obtenerNodo(nodo *nodoArbol[K, V], clave K) *no
 	return nodo
 }
 
+func (abb *abbDiccionario[K, V]) Pertenece(clave K) bool {
+	return abb.busquedaRecursiva(abb.raiz, clave)
+}
+
 func (abb *abbDiccionario[K, V]) Obtener(clave K) V {
 	nodo := abb.obtenerNodo(abb.raiz, clave)
 	if nodo == nil {
@@ -85,50 +85,11 @@ func (abb *abbDiccionario[K, V]) Obtener(clave K) V {
 
 // Casos: sin hojas, 1 hoja, 2 hojas.
 
-func (abb *abbDiccionario[K, V]) borrarRec(nodo *nodoArbol[K, V], clave K) (*nodoArbol[K, V], V) {
-	if nodo == nil {
-		panic("El arbol esta vacio")
-	}
-	comparacion := abb.cmp(clave, nodo.clave)
-	var dato V
-	if comparacion < 0 {
-		nodo.hijo_izq, dato = abb.borrarRec(nodo.hijo_izq, clave)
-		return nodo, dato
-	} else if comparacion > 0 {
-		nodo.hijo_der, dato = abb.borrarRec(nodo.hijo_der, clave)
-		return nodo, dato
-	}
-	abb.cantidad--
-
-	// No tengo hijos
-	if nodo.hijo_izq == nil && nodo.hijo_der == nil {
-		return nil, nodo.dato
-	}
-	// Caso un solo hijo
-	if nodo.hijo_der == nil {
-		return nodo.hijo_izq, nodo.dato
-	} else if nodo.hijo_izq == nil {
-		return nodo.hijo_der, nodo.dato
-	}
-
-	// Tengo 2 hijos. Aca busco el minimo del lado izquierdo del sub-arbol derecho
-	sucesor := abb.buscarMin(nodo.hijo_der)
-	nodo.clave = sucesor.clave
-	nodo.dato = sucesor.dato
-	nodo.hijo_der, _ = abb.borrarRec(nodo.hijo_der, sucesor.clave)
-	return nodo, sucesor.dato
-}
-
-func (abb *abbDiccionario[K, V]) buscarMin(nodo *nodoArbol[K, V]) *nodoArbol[K, V] {
-	for nodo.hijo_izq != nil {
-		nodo = nodo.hijo_izq
-	}
-	return nodo
-}
-
 func (abb *abbDiccionario[K, V]) Borrar(clave K) V {
 	var dato V
-	abb.raiz, dato = abb.borrarRec(abb.raiz, clave)
+	if abb.cantidad == 0 {
+		panic("El arbol esta vacio")
+	}
 	return dato
 }
 
@@ -136,12 +97,11 @@ func (abb *abbDiccionario[K, V]) Cantidad() int {
 	return abb.cantidad
 }
 
-// Queda este mostro que nunca me sale
-
 func (abb *abbDiccionario[K, V]) Iterar(visitar func(clave1 K, dato V) bool) {
 }
 
-type iteradorDiccionario[K comparable, V any] struct{}
+type iteradorDiccionario[K comparable, V any] struct {
+}
 
 func (abb *abbDiccionario[K, V]) Iterador() IterDiccionario[K, V] {
 	return iteradorDiccionario[K, V]{}
@@ -152,6 +112,7 @@ func (abb iteradorDiccionario[K, V]) HaySiguiente() bool {
 }
 
 func (abb iteradorDiccionario[K, V]) Siguiente() {
+
 }
 
 func (abb iteradorDiccionario[K, V]) VerActual() (K, V) {
